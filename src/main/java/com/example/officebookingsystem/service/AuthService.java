e72373a73dbf22d9b41517f850e2327e7d4e0f18
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-06-02 12:38:58 +0700

    Fixing Code Architecture

--- src/main/java/com/example/officebookingsystem/service/AuthService.java
+++ src/main/java/com/example/officebookingsystem/service/AuthService.java
@@ -94,8 +94,11 @@ public class AuthService {
                     }
             );
         }
+
+
         user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
+
 }
